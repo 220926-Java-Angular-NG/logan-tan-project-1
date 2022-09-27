9308a627badbe0ba92b2732a342daaad7cbd746e
Logan <logan.tan@wsu.edu>
2022-09-27 08:21:07 -0700

    adding in fuctionality to add new users
    empty add and find functions to decide on structure

--- Database.java
+++ Database.java
@@ -1,5 +1,17 @@
 public class Database {
-    public Database(){
+    public Database() {
+        //if there was a file to load initialize here
+    }
 
+    public void createUser(String UID, String Pass, int type) {
+        Users temp = new Users(UID, Pass, type);
+        add(temp);
+    }
+
+    public void add(Users ad) {
+
+    }
+    public Users find(String username){
+        return null;
     }
 }
